53bcca9ead1c89217eada98658804caa7fc399bf
dismas <afubwa@gmail.com>
2023-02-17 15:00:03 +0400

    React-Hooks-Components-Lab

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,23 @@ function Home() {
 
 {/* write an <About> component here */}
 
+function About(){
+  return(
+    <div id="about">
+      <p>About</p>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
+      {/* <Home />    component here */}
+      <Home  />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
